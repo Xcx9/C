03399823c8e3d9f67de010becf6d50fc5e160db1
Shlepabot <114918818+Xcx9@users.noreply.github.com>
2026-02-19 14:53:22 +0300

    Update Plant.cs

--- C#/lab_2/Plant.cs
+++ C#/lab_2/Plant.cs
@@ -8,27 +8,33 @@ namespace ConsoleApp2
 {
     internal class Plant:Organism
     {
-        private int illumination;
+        private string _name;
+        private double _illumination;
 
-        public Plant() { }
-        public Plant(string name) { }
+        public Plant() { _name = "Unknown"; _illumination = 1.0; }
+        public Plant(string name, int illumination = 0) { }
 
+
+        private void checkIllumination()
+        {
+
+        }
 
         public override int checkEnergy()
         {
-            switch (illumination)
+            switch (_illumination)
             {
-                case 0: return 10;
-                case 1: return 9;
-                case 2: return 8;
-                case 3: return 7;
-                case 4: return 6;
-                case 5: return 5;
-                case 6: return 4;
-                case 7: return 3;
-                case 8: return 2;
-                case 9: return 1;
-                case 10: return 0;
+                case 1: return 10;
+                case 0.9: return 9;
+                case 0.8: return 8;
+                case 0.7: return 7;
+                case 0.6: return 6;
+                case 0.5: return 5;
+                case 0.4: return 4;
+                case 0.3: return 3;
+                case 0.2: return 2;
+                case 0.1: return 1;
+                case 0: return 0;
                 default: return -1;
             }
         }
