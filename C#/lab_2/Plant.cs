c113513e54ae0196b112d093aacd0dc00c51ef0a
Shlepabot <114918818+Xcx9@users.noreply.github.com>
2026-03-04 23:01:30 +0300

    Update Plant.cs

--- C#/lab_2/Plant.cs
+++ C#/lab_2/Plant.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp2
 {
-    internal class Plant : Organism
+    internal class Plant : Organism, ILogInfo
     {
         private string _name;
+        private int _age;
+        private int energy;
         private double _illumination;
+        public readonly string organism_type = "Plant";
 
-        public Plant() { _name = "Unknown"; _illumination = 1.0; }
-        public Plant(string name, int illumination = 0) { _name = name; _illumination = illumination; }
-
+        public Plant(string name, int age, int illumination = 0) : base("Растение", name, age)
+        { _name = name; 
+            age = 0; 
+            _illumination = illumination / 10; energy = illumination;
+        }
 
         public void checkIllumination()
         {
@@ -34,27 +40,24 @@ namespace ConsoleApp2
                     Console.WriteLine($"Ошибка! Введите число от {1} до {10}.");
                 }
             } while (!isValid);
+            energy = value;
             _illumination = value / 10;
             Console.WriteLine($"Теперь уровень освещения {_name}: {_illumination}");
         } 
 
         public override int checkEnergy()
         {
-            switch (_illumination)
-            {
-                case 1: return 10;
-                case 0.9: return 9;
-                case 0.8: return 8;
-                case 0.7: return 7;
-                case 0.6: return 6;
-                case 0.5: return 5;
-                case 0.4: return 4;
-                case 0.3: return 3;
-                case 0.2: return 2;
-                case 0.1: return 1;
-                case 0: return 0;
-                default: return -1;
-            }
+                return energy;
+        }
+
+        public override string ToString()
+        {
+            return $"Растение: {_name}, Уровень освещения: {_illumination}";
+        }
+
+        public string LogInfo()
+        {
+            return $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Статус: {GetStatus()}";
         }
     }
 }
