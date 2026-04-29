8a1b092edd92413edb1046882134ad513143e6a7
Xcx <114918818+Xcx9@users.noreply.github.com>
2026-04-29 23:40:57 +0300

    Create Config.cs

---
+++ C#/lab5/Config.cs
@@ -0,0 +1,24 @@
+namespace Lab5_AsyncParallel
+{
+    static class Config
+    {
+        public const int ThrottleMaxParallel = 4;
+        public const int TotalTasks = 500;
+        public const int MinTaskDurationMs = 100;
+        public const int MaxTaskDurationMs = 500;
+        public const int BlockCount = 50;
+        public const int BlockSize = 1024;
+        public const int CrcLoopIterations = 100_000;
+        public const int CompressionDelayMs = 50;
+
+        public const int RetryMaxAttempts = 3;
+        public static readonly TimeSpan[] RetryBackoff =
+        {
+            TimeSpan.FromSeconds(1),
+            TimeSpan.FromSeconds(2),
+            TimeSpan.FromSeconds(4)
+        };
+
+        public const int CacheParallelRequests = 20;
+    }
+}
