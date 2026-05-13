54571f505db7c38b93ad6450c109cdb053fb60f0
Xcx <114918818+Xcx9@users.noreply.github.com>
2026-05-13 20:19:14 +0300

    Create Program.cs

---
+++ C#/lab6/Program.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.ReactiveUI;
+using System;
+
+namespace ThreadProfiler
+{
+    class Program
+    {
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .UseReactiveUI();
+    }
+}
